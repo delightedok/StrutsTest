9e8f7e43dcba3f514fdc40cc570f8c60d05a0b77
Johnny Tam Gitsun <iamdajieshen@gmail.com>
2016-07-07 18:56:40 +0800

    My First Commit Improve

--- WebContent/WEB-INF/views/typeconvert/input.jsp
+++ WebContent/WEB-INF/views/typeconvert/input.jsp
@@ -18,5 +18,27 @@
 	</tr>
 </s:form>
 
+<s:form action="typeconvert-login_map">
+	<s:textfield name="users['one'].name" label="the one username"></s:textfield>
+	<s:textfield name="users['one'].pass" label="the one password"></s:textfield>
+	<s:textfield name="users['two'].name" label="the two username"></s:textfield>
+	<s:textfield name="users['two'].pass" label="the two password"></s:textfield>
+	<tr>
+		<td colspan="2"><s:submit value="convert" theme="simple"></s:submit></td>
+		<s:reset value="reset" theme="simple"></s:reset>
+	</tr>
+</s:form>
+
+<s:form action="typeconvert-login_list">
+	<s:textfield name="usersl[0].name" label="the 0 username"></s:textfield>
+	<s:textfield name="usersl[0].pass" label="the 0 password"></s:textfield>
+	<s:textfield name="usersl[1].name" label="the 1 username"></s:textfield>
+	<s:textfield name="usersl[1].pass" label="the 1 password"></s:textfield>
+	<tr>
+		<td colspan="2"><s:submit value="convert" theme="simple"></s:submit></td>
+		<s:reset value="reset" theme="simple"></s:reset>
+	</tr>
+</s:form>
+
 </body>
 </html>
--- src/com/tamgitsun/struts2/typeconvert/LoginAction.java
+++ src/com/tamgitsun/struts2/typeconvert/LoginAction.java
@@ -1,17 +1,28 @@
 package com.tamgitsun.struts2.typeconvert;
 
+import java.util.List;
+import java.util.Map;
+
 import com.opensymphony.xwork2.Action;
 
 public class LoginAction implements Action{
 	
 	private User user;
 	private String tip;
+	private Map<String,User> users;
+	private List<User> usersl;
 	
 	public void setUser(User user){
 		this.user=user;
 	}
 	public void setTip(String tip){
 		this.tip=tip;
+	}
+	public void setUsers(Map<String,User> users){
+		this.users=users;
+	}
+	public void setUsersl(List<User> usersl){
+		this.usersl=usersl;
 	}
 	
 	public User getUser(){
@@ -20,11 +31,44 @@ public class LoginAction implements Action{
 	public String getTip(){
 		return tip;
 	}
+	public Map<String,User> getUsers(){
+		return users;
+	}
+	public List<User> getUsersl(){
+		return usersl;
+	}
 	
 	public String execute() throws Exception{
+		return "success";
+	}
+	
+	public String login() throws Exception{
 		System.out.println("username:"+user.getName());
 		System.out.println("password:"+user.getPass());
 		return "success";
 	}
-
+	
+	public String login_map() throws Exception{
+		System.out.println(getUsers());
+		if(getUsers().get("one").getName().equals("tamgitsun")&&getUsers().get("one").getPass().equals("123456")){
+			setTip("登录成功");
+			return "success";
+		}else{
+			setTip("登录失败");
+			return "error";
+		}
+	}
+	
+	public String login_list() throws Exception{
+		System.out.println(getUsersl());
+		User u=getUsersl().get(0);
+		if(u.getName().equals("tamgitsun")&&u.getPass().equals("123456")){
+			setTip("登录成功");
+			return "success";
+		}else{
+			setTip("登录失败");
+			return "error";
+		}
+	}
+	
 }
